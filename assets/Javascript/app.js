01da36554a6dad0b2335a78ba7ad79efbafd084a
moltenshark <alex@berez.com>
2017-10-26 20:33:52 -0500

    double-checks ingredients before failing

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -2,20 +2,32 @@ $(function(){
 
     var yummlyID = "_app_id=3123c164", yummlyKey = "_app_key=0a453b6219d75c4f9b5bd7deafcd8724";
 
-    function recipeSearch(cuisine, course, ingredient) {
+    function recipeSearch(cuisine, course, ingredient, math) {
         var ingredientSearch = "";
         if (ingredient) {
             var ingredientSearch = `&allowedIngredient[]=${ingredient}`;
         }
-        let queryURL = `http://api.yummly.com/v1/api/recipes?${yummlyID}&${yummlyKey}&requirePictures=true&maxResult=4&start=${~~(Math.random() * 50)}&allowedCuisine[]=cuisine^cuisine-${cuisine}&allowedCourse[]=course^course-${course}${ingredientSearch}`;
+        let queryURL = `http://api.yummly.com/v1/api/recipes?${yummlyID}&${yummlyKey}&requirePictures=true&maxResult=4&start=${math}&allowedCuisine[]=cuisine^cuisine-${cuisine}&allowedCourse[]=course^course-${course}${ingredientSearch}`;
         console.log(queryURL);
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).done(function(response) {
+        }).done(function(response) { 
             let result = response.matches;
-            for (let index of result) {
-                recipeReturn(index.id)
+            console.log(result.length);
+            if (!result.length && math) {
+                console.log("redo");
+                recipeSearch(cuisine, course, ingredient, 0);
+            }
+            else if (!result.length && !math) {
+                let div = $(`<div class="col-xs-6">`);
+                div.append(`<h3>No results found. Please try a different ingredient!</h3>`);
+                $("#recipeHome").append(div);
+            }
+            else {
+                for (let index of result) {
+                    recipeReturn(index.id)
+                }
             }
         });
     }
@@ -33,9 +45,9 @@ $(function(){
     }
 
     $(document).on("click", "#makeIt", function() {
-        let cuisine = $("#cuisineChoice").val().toLowerCase().replace(/\s+/g, ''), course = $("#courseChoice").val().replace(/\s+/g, ''), ingredient = $("#ingredientChoice").val().toLowerCase().replace(/\s+/g, '');
+        let cuisine = $("#cuisineChoice").val().toLowerCase().replace(/\s+/g, ''), course = $("#courseChoice").val().replace(/\s+/g, ''), ingredient = $("#ingredientChoice").val().toLowerCase().replace(/\s+/g, ''), math = ~~(Math.random() * 50);
         $("#recipeHome").empty();
-        recipeSearch(cuisine, course, ingredient);
+        recipeSearch(cuisine, course, ingredient, math);
     });
 
 });
