b9dfca8e371105788d079aa775ffe2409f154d90
moltenshark <alex@berez.com>
2017-10-26 19:30:28 -0500

    can now pick ingredients

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -3,7 +3,7 @@ $(function(){
     var yummlyID = "_app_id=3123c164", yummlyKey = "_app_key=0a453b6219d75c4f9b5bd7deafcd8724";
 
     function recipeSearch(cuisine, course, ingredient) {
-        let queryURL = `http://api.yummly.com/v1/api/recipes?${yummlyID}&${yummlyKey}&requirePictures=true&maxResult=4&start=${~~(Math.random() * 50)}&allowedCuisine[]=cuisine^cuisine-${cuisine}&allowedCourse[]=course^course-${course}`;
+        let queryURL = `http://api.yummly.com/v1/api/recipes?${yummlyID}&${yummlyKey}&requirePictures=true&maxResult=4&start=${~~(Math.random() * 50)}&allowedCuisine[]=cuisine^cuisine-${cuisine}&allowedCourse[]=course^course-${course}&allowedIngredient[]=${ingredient}`;
         console.log(queryURL);
         $.ajax({
             url: queryURL,
@@ -29,9 +29,9 @@ $(function(){
     }
 
     $(document).on("click", "#makeIt", function() {
-        let cuisine = $("#cuisineChoice").val().toLowerCase().replace(/\s+/g, ''), course = $("#courseChoice").val().replace(/\s+/g, '');
+        let cuisine = $("#cuisineChoice").val().toLowerCase().replace(/\s+/g, ''), course = $("#courseChoice").val().replace(/\s+/g, ''), ingredient = $("#ingredientChoice").val().toLowerCase().replace(/\s+/g, '');
         $("#recipeHome").empty();
-        recipeSearch(cuisine, course);
+        recipeSearch(cuisine, course, ingredient);
     });
 
 });
