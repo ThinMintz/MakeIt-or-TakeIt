9be867536274cce241cac2db960063790336cefc
moltenshark <alex@berez.com>
2017-11-02 17:25:04 -0500

    local staorage zip code

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -71,6 +71,11 @@ $(function(){
 
     //Will's coding portion
 
+    (function () {
+        let zipCode = localStorage.getItem("zipCode");
+        if (zipCode) $("#zipcode").attr("value", zipCode);
+    })();
+
     function foursquareApi(userZip, userCuisineInput){
         let clientId = "MKSDIAW4TPUGKG2VZVL1AWA3Y0RRNE4IL1DBEGOUTE5V4GWD", clientSecret = "QOSAAQPN015GOWGVZ5XOOCSE5LERPN5KBZRACT4DJNN1MBUG",
         queryURL = `https://api.foursquare.com/v2/venues/search?v=20161016&near=${userZip}&query=${userCuisineInput}food&intent=checkin&limit=10&client_id=${clientId}&client_secret=${clientSecret}`;
@@ -108,10 +113,9 @@ $(function(){
         let userZip = $("#zipcode").val().trim(), userCuisineInput = $("#cuisineChoice").val();
         $("#places").empty();
         validText(userZip, false)
-            ? ($("#recipeHome").html(`<div class="col-xs-12"><h3>Searching for restaurants<span class="ellipsis-anim"><span>.</span><span>.</span><span>.</span></span></h3></div>`), foursquareApi(userZip, userCuisineInput))
+            ? ($("#recipeHome").html(`<div class="col-xs-12"><h3>Searching for restaurants<span class="ellipsis-anim"><span>.</span><span>.</span><span>.</span></span></h3></div>`), localStorage.setItem("zipCode", userZip), foursquareApi(userZip, userCuisineInput))
             : $("#recipeHome").html(`<div class="col-xs-12"><h3>Please input a valid ZIP Code!</h3></div>`);
     });
 
 
 });
-
